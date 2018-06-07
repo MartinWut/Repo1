95f4a2e70c2a3555ab6e98740a5431c4f34003c2
MartinWut <martinwutke@hotmail.de>
2018-06-07 08:14:00 +0200

    Alternativer Weg über loopen einer einzigen Abfrage-Funktion

--- Version_0.1.R
+++ Version_0.1.R
@@ -229,3 +229,131 @@ faculty_semCompare <- function(faculty_vec, semester){
   return(means_df[,c(1,3)])
 }
 
+
+
+
+
+
+
+
+
+#################################################
+#### Alternativ: eine Funktion -> gelooped ######
+#################################################
+
+
+
+### Creating a Dataframe of the semesternumber
+
+semester_vec <- fromJSON(readLines("https://pruefungsverwaltung.uni-goettingen.de/statistikportal/api/dropdownvalues?_dc=1524293694665&type=SEMESTERNR&selectAllDummy=false&forQueryId=215&page=1&start=0&limit=25"))
+
+### Creating a Dataframe of the faculties
+
+faculty_vec <- fromJSON(readLines("https://pruefungsverwaltung.uni-goettingen.de/statistikportal/api/dropdownvalues?_dc=1524293694760&type=FAK&path=&selectAllDummy=false&forQueryId=215&page=1&start=0&limit=25"))
+
+### Result
+
+semester_vec
+faculty_vec
+sort(faculty_vec[,2])
+
+
+
+##############################################################
+##### Creating the data for the faculty specific modules #####
+##############################################################
+
+
+### Creating a Dataframe of the semesternumber
+
+semester_vec <- fromJSON(readLines("https://pruefungsverwaltung.uni-goettingen.de/statistikportal/api/dropdownvalues?_dc=1524293694665&type=SEMESTERNR&selectAllDummy=false&forQueryId=215&page=1&start=0&limit=25"))
+
+### Creating a Dataframe of the faculties
+
+faculty_vec <- fromJSON(readLines("https://pruefungsverwaltung.uni-goettingen.de/statistikportal/api/dropdownvalues?_dc=1524293694760&type=FAK&path=&selectAllDummy=false&forQueryId=215&page=1&start=0&limit=25"))
+
+### Result
+
+semester_vec
+faculty_vec
+sort(faculty_vec[,2])
+
+## create a vector with the module-URL for each faculty
+
+url <- "https://pruefungsverwaltung.uni-goettingen.de/statistikportal/api/dropdownvalues?_dc=1524294424781&type=STUDIENMODUL&path=FAK%253D8&selectAllDummy=false&forQueryId=215&page=1&start=0&limit=25"
+
+url_part1 <- substr(url, start=1, stop=130)
+url_part2 <- substr(url, start=132, stop=nchar(url))
+
+module_vec <- rep(NA, length(faculty_vec[,2]))
+
+for (i in 1:length(faculty_vec[,2])) {
+  module_vec[i] <- paste(url_part1, sort(faculty_vec[,2])[i] , url_part2, sep = "")
+}
+
+module_vec
+
+## create a list with dataframes for each faculty which contain the information for the faculty specific modules
+
+all_modules <- list(NA)
+
+for (i in 1:length(faculty_vec[,2])) {
+  all_modules[[i]] <- fromJSON(readLines(module_vec[i]))
+}
+
+names(all_modules) <- c(arrange(faculty_vec, value)[,1])
+
+
+
+
+single_request <- function(Semester, Fakultät, Modul){
+  resultsURL <- "https://pruefungsverwaltung.uni-goettingen.de/statistikportal/api/queryexecution/results"
+  requestJSON <- readChar("request.json", file.info("request.json")$size)
+  #modulesDataFrame <- all_modules[[Fakultät]] #### hier die gewünschte Fakultät angegeben. Bsp.: all_modules$`Theologische Fakultät`
+  
+  records <- data.frame(matrix(nrow = 0, ncol = 21))
+  
+  facultyString <- paste0('"lastValue":"',subset(faculty_vec[,2], faculty_vec[,1] == Fakultät | faculty_vec[,2] == Fakultät) , '"') ### Hier den Wert der Fakultät angeben
+  thisRequestJSON <- sub('"lastValue":"12"', facultyString, requestJSON)
+  
+  moduleString <- paste0('"lastValue":"', Modul, '"') ### Hier den Wert des Modules angeben
+  thisRequestJSON <- sub('"lastValue":"112"', moduleString, requestJSON)
+  
+  semesterString <- paste0('"lastValue":"', subset(semester_vec[,2], semester_vec[,1] == Semester), '"') ### Hier den Wert des Semesters angeben 
+  thisRequestJSON <- sub('"lastValue":"60"', semesterString, thisRequestJSON)
+  
+  bodyList <- list(data = thisRequestJSON)
+  request <- POST(resultsURL, body = bodyList, encode = "form")
+  stop_for_status(request)
+  
+  responseJSON <- content(request, encoding = "UTF-8", type = "text")
+  responseDataFrame <- fromJSON(responseJSON)$data$records
+  results <<- data.frame(matrix(nrow = max(length(Semester),length(Fakultät), length(Modul)), ncol = 21))
+  results <- responseDataFrame
+  #results <- results[,c(17,12,2,15,20,14,3,18,5,4,6,21,7,16,1,8,13,19,9,11,10)]
+  return(results)
+}
+
+
+test <- single_request("WS 2016/2017", 14, 104)
+test
+
+## Example: all semesters: "Produktion und Logistik (Wiwi-Fakultät (14)) (Value 115)
+
+res <- data.frame(matrix(nrow = length(semester_vec[,1]), ncol = 21))
+for (i in 1:length(semester_vec[,1])) {
+  if (class(single_request(semester_vec[i,1], 14, 115)) == "data.frame") {
+    res[i,] <- single_request(semester_vec[i,1], 14, 115)
+  }
+}
+
+View(res) # Result
+res$
+
+# compute the mean
+res$X18
+str(res$X18)
+tmp <- subset(as.numeric(res$X18), as.numeric(res$X18) != "NA")
+mean(tmp)
+
+
